7f68d880c68b24916b465d3808f22051a1fe6f88
CalixtoTheBugHunter <klixto@outlook.com>
2021-05-01 00:00:24 -0300

    remove unused class

--- src/components/atoms/BaseInput/BaseInput.vue
+++ src/components/atoms/BaseInput/BaseInput.vue
@@ -1,127 +1,127 @@
 <template>
-  <div class="BaseInput" :class="BaseInputClass">
-    <label v-if="!!label" class="BaseInput__label" :class="labelClass">
-      {{ label }}
-    </label>
-    <input
-      ref="input"
-      class="BaseInput__input"
-      v-bind="$attrs"
-      :value="value"
-      @focus="onFocus"
-      @blur="onLostFocus"
-      @input="debounceInput"
-      @keyup="onKeyup"
-      @keyup.enter="inputEnterHandler"
-    />
+    <div class="BaseInput" :class="BaseInputClass">
+        <label v-if="!!label" class="BaseInput__label">
+            {{ label }}
+        </label>
+        <input
+            ref="input"
+            class="BaseInput__input"
+            v-bind="$attrs"
+            :value="value"
+            @focus="onFocus"
+            @blur="onLostFocus"
+            @input="debounceInput"
+            @keyup="onKeyup"
+            @keyup.enter="inputEnterHandler"
+        />
 
-    <p v-if="isInvalid" class="BaseInput__invalidLabel">
-      {{ invalidLabel }}
-    </p>
-    <slot />
-  </div>
+        <p v-if="isInvalid" class="BaseInput__invalidLabel">
+            {{ invalidLabel }}
+        </p>
+        <slot />
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'BaseInput',
-  props: {
-    value: {
-      type: [String, Number],
-      required: true
+    name: 'BaseInput',
+    props: {
+        value: {
+            type: [String, Number],
+            required: true
+        },
+        label: {
+            type: String,
+            default: ''
+        },
+        debounceTime: {
+            type: Number,
+            default: 500
+        },
+        isInvalid: {
+            type: Boolean,
+            default: false
+        },
+        invalidLabel: {
+            type: String,
+            default: ''
+        }
     },
-    label: {
-      type: String,
-      default: ''
+    data() {
+        return {
+            debounce: null,
+            focus: false
+        }
     },
-    debounceTime: {
-      type: Number,
-      default: 500
+    computed: {
+        BaseInputClass() {
+            return { 'BaseInput--invalid': !!this.isInvalid }
+        },
     },
-    isInvalid: {
-      type: Boolean,
-      default: false
-    },
-    invalidLabel: {
-      type: String,
-      default: ''
+    methods: {
+        onFocus() {
+            this.focus = !this.focus
+        },
+        onLostFocus() {
+            this.onFocus()
+            this.$emit('blur')
+        },
+        onKeyup(e) {
+            let value = e ? e.target.value : this.value
+            this.$emit('input', value)
+        },
+        setFocus() {
+            this.$refs.input.focus()
+        },
+        debounceInput(e) {
+            const val = e.target.value
+            this.$emit('input', val)
+            clearTimeout(this.debounce)
+            this.debounce = setTimeout(() =>
+                this.$emit('debounce', val),
+                this.debounceTime
+            )
+        },
+        inputEnterHandler() {
+            this.$emit('enter')
+        }
     }
-  },
-  data() {
-    return {
-      debounce: null,
-      focus: false
-    }
-  },
-  computed: {
-    BaseInputClass() {
-      return { 'BaseInput--invalid': !!this.isInvalid }
-    },
-  },
-  methods: {
-    onFocus() {
-      this.focus = !this.focus
-    },
-    onLostFocus() {
-      this.onFocus()
-      this.$emit('blur')
-    },
-    onKeyup(e) {
-        let value = e ? e.target.value : this.value
-        this.$emit('input', value)
-    },
-    setFocus() {
-      this.$refs.input.focus()
-    },
-    debounceInput(e) {
-      const val = e.target.value
-      this.$emit('input', val)
-      clearTimeout(this.debounce)
-      this.debounce = setTimeout(
-        () => this.$emit('debounce', val),
-        this.debounceTime
-      )
-    },
-    inputEnterHandler() {
-      this.$emit('enter')
-    }
-  }
 }
 </script>
 
 <style lang="scss" scoped>
 .BaseInput {
-  position: relative;
-  &[disabled] {
-    opacity: 0.5;
-    cursor: not-allowed;
-    color: $color-dark;
-    .BaseInput__input {
-      background-color: $color-light;
+    position: relative;
+    &[disabled] {
+        opacity: 0.5;
+        cursor: not-allowed;
+        color: $color-dark;
+        .BaseInput__input {
+            background-color: $color-light;
+        }
     }
-  }
-  &__label {
-    color: $color-light;
-    font-size: 17px;
-    transition: transform 0.2s;
-    pointer-events: none;
-  }
-  &__input {
-    border: 1px solid $color-light;
-    padding: 9px 0;
-    width: 100%;
-    background: transparent;
-    .BaseInput--invalid & {
-      color: $color-primary;
-      border-bottom: 1px solid $color-primary;
+    &__label {
+        color: $color-light;
+        font-size: 16px;
+        transition: transform 0.2s;
+        pointer-events: none;
     }
-  }
-  &__invalidLabel {
-    padding: 5px 10px;
-    font-size: 12px;
-    color: $color-primary;
-    font-weight: 900;
-    position: absolute;
-  }
+    &__input {
+        border: 1px solid $color-light;
+        padding: 9px 0;
+        width: 100%;
+        background: transparent;
+        .BaseInput--invalid & {
+            color: $color-primary;
+            border-bottom: 1px solid $color-primary;
+        }
+    }
+    &__invalidLabel {
+        padding: 5px 10px;
+        font-size: 12px;
+        color: $color-primary;
+        font-weight: 900;
+        position: absolute;
+    }
 }
 </style>
