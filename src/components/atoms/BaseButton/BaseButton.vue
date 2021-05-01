04fdc4d6a659ea0fafdb2b7329739c0985ee563d
CalixtoTheBugHunter <klixto@outlook.com>
2021-04-30 22:36:09 -0300

    button atom

---
+++ src/components/atoms/BaseButton/BaseButton.vue
@@ -0,0 +1,67 @@
+<template>
+  <component
+    :is="tag"
+    type="button"
+    v-bind="$attrs"
+    class="BaseButton"
+    :disabled="disabled"
+    @click="click"
+  >
+    <span v-if="!!label" class="BaseButton__Text">
+      {{ label }}
+    </span>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    tag: {
+      type: String,
+      default: 'button',
+      validator: val => ['button', 'a'].includes(val)
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    click(e) {
+      this.$emit('click', e)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.BaseButton {
+  height: 40px;
+  -webkit-appearance: none;
+  display: flex;
+  background: inherit;
+  padding: 0 10px;
+  cursor: pointer;
+  align-items: center;
+  position: relative;
+  white-space: nowrap;
+  background: $color-primary;
+  border-radius: 10px;
+  border: 0 none;
+  &:hover {
+    filter: brightness(90%) contrast(150%);
+  }
+  &__Text {
+    font-size: 16px;
+    color: $color-white;
+    text-align: center;
+    display: block;
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/atoms/index.js
@@ -0,0 +1,5 @@
+import BaseButton from './BaseButton/BaseButton.vue'
+
+export {
+    BaseButton
+}
